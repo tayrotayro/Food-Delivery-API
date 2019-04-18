0c44a59f4f065e480bf683ba998c635ae9a3a901
Tam Nguyen <tayrothepro@gmail.com>
2019-04-17 19:55:00 -0500

    updates

--- routes/authorization.js
+++ routes/authorization.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const Driver = require('../models/Driver');
+const Owner = require('../models/Owner');
 
 router.post('/api/signin', (req, res) => {
     User.findOne({ email: req.body.email })
         .then((user) => {
             if (!user) {
-                res.status(401).send({
-                    error: "User authentication failed: user doesn't exist!"
+                res.send({
+                    error: "The provided email is not registered"
                 })
             } else {
                 if (bcrypt.compareSync(req.body.password, user.password)) {
@@ -21,12 +23,47 @@ router.post('/api/signin', (req, res) => {
                         data: user
                     })
                 } else {
-                    res.status(401).send("User authentication failed: wrong password!");
+                    res.send({
+                        error: "Your provided password is incorrect"
+                    });
                 }
             }
         })
         .catch((err) => {
-            res.status(401).send("User authentication failed");
+            res.send({
+                error: "User authentication failed!"
+            });
+        })
+})
+
+router.get('/api/user-roles/:baseUserId', (req, res) => {
+    const baseUserId = req.params.baseUserId;
+    let availableRoles = [1];
+    // 1 as User, 2 as Driver and 3 as Owner
+    // Having 1 as default because everyone has a role as a User
+
+    Promise.all([
+        Driver.findOne({ baseUserId }),
+        Owner.findOne({ baseUserId })
+    ])
+        .then(([driver, owner]) => {
+            if (driver) {
+                availableRoles.push(2);
+            }
+
+            if (owner) {
+                availableRoles.push(3);
+            }
+
+            res.send({
+                message: "Successfully get user's available roles",
+                data: availableRoles
+            })
+        })
+        .catch(err => {
+            res.send({
+                error: err.message
+            })
         })
 })
 
